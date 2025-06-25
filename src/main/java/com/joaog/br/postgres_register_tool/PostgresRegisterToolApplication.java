5ee7b85f7f9b5debbad9f3f6ac016eaf1fd81bf1
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-06-24 21:20:16 -0300

    first commit

---
+++ src/main/java/com/joaog/br/postgres_register_tool/PostgresRegisterToolApplication.java
@@ -0,0 +1,13 @@
+package com.joaog.br.postgres_register_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresRegisterToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresRegisterToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaog/br/postgres_register_tool/PostgresRegisterToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaog.br.postgres_register_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresRegisterToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
