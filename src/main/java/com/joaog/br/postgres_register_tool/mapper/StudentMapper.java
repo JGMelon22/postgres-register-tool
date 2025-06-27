880349b151594875aa38ff4458dbffcfca2673d1
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-06-26 21:49:02 -0300

    chore: include MapStruct package and develop base mapping interface

---
+++ src/main/java/com/joaog/br/postgres_register_tool/mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.joaog.br.postgres_register_tool.mapper;
+
+import com.joaog.br.postgres_register_tool.dto.StudentRequest;
+import com.joaog.br.postgres_register_tool.dto.StudentResponse;
+import com.joaog.br.postgres_register_tool.model.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface StudentMapper {
+
+    Student toEntity(StudentRequest student);
+
+    StudentResponse toResponse(Student student);
+
+    void applyUpdate(StudentRequest studentRequest, @MappingTarget Student student);
+}
