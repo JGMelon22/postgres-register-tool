49494a19d6d62559b44803429177831e4054bd03
JGMelon22 <jgvasconcellosm22@outlook.com>
2025-07-06 12:37:30 -0300

    refactor: ignore Id property

--- src/main/java/com/joaog/br/postgres_register_tool/mapper/StudentMapper.java
+++ src/main/java/com/joaog/br/postgres_register_tool/mapper/StudentMapper.java
@@ -4,6 +4,7 @@ import com.joaog.br.postgres_register_tool.dto.StudentRequest;
 import com.joaog.br.postgres_register_tool.dto.StudentResponse;
 import com.joaog.br.postgres_register_tool.model.Student;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import java.util.ArrayList;
@@ -11,11 +12,13 @@ import java.util.ArrayList;
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
 
+    @Mapping(target = "id", ignore = true)
     Student toEntity(StudentRequest student);
 
     StudentResponse toResponse(Student student);
 
     ArrayList<StudentResponse> toResponse(Iterable<Student> students);
 
+    @Mapping(target = "id", ignore = true)
     void applyUpdate(StudentRequest studentRequest, @MappingTarget Student student);
 }
